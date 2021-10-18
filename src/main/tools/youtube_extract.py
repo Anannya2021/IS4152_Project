54e322b3f4a73216417c20a55fbeaef7841ea2dc
Akanksha Bansal <77793414+akanksha-b14@users.noreply.github.com>
2021-10-18 21:52:17 +0800

    Updated code to slice 1 minute from the audio clip

--- src/main/tools/youtube_extract.py
+++ src/main/tools/youtube_extract.py
@@ -3,6 +3,7 @@ import os
 import csv
 import pandas as pd
 import ffmpeg
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 links_meta = pd.read_csv('youtube_links.csv')
 
@@ -11,11 +12,18 @@ links_meta_list = links_meta['Links'].values.tolist()
 for i in links_meta_list:
     yt = YouTube(i)
     video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path=".")
+    out_file = video.download(output_path="raw_audio_files_sad")
+    
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
-    audio_input = ffmpeg.input(new_file)
-    audio_cut = audio_input.audio.filter('atrim', duration=60)
-    audio_output = ffmpeg.output(audio_cut, base+'_trim.mp3')
-    ffmpeg.run(audio_output)
+
+    ffmpeg_cmd = (
+            ffmpeg
+                .input(new_file, ss=0, t=60)
+                .output(base+'_trim.mp3', acodec='mp3')
+                .overwrite_output()
+        )
+    ffmpeg_cmd.run()
+    
+    
