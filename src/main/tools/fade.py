1b0bf3ec793ce669afd20ad3ea8d31ceaaefac8c
Akanksha Bansal <77793414+akanksha-b14@users.noreply.github.com>
2021-11-13 23:26:09 +0800

    Update fade.py

--- src/main/tools/fade.py
+++ src/main/tools/fade.py
@@ -1,9 +1,13 @@
 import os
 import pydub
 from pydub import AudioSegment
+import sys
 
-for filename in os.listdir('./Happy'):
+folder_name = sys.argv[1]
+output_folder = sys.argv[2]
+
+for filename in os.listdir('./'+folder_name):
     if filename.endswith(".mp3"):
-        song = AudioSegment.from_mp3('./Happy/'+filename)
+        song = AudioSegment.from_mp3('./'+folder_name+'/'+filename)
         faded_song = song.fade_in(1000).fade_out(3000)
-        faded_song.export('./Happy_fade/'+filename, format="mp3")
+        faded_song.export('./'+output_folder+'/'+filename, format="mp3")
